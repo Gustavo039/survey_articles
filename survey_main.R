8806d6232d873c3d1abe921d0b65bf13c08453d7
Gustavo Almeida Silva <gustavoaeida2002@gmail.com>
2023-07-10 23:47:01 -0300

    work done buddie. heyayyyy

--- survey_main.R
+++ survey_main.R
@@ -220,3 +220,227 @@ cluster_2_stage_schools_classes = survey::svydesign(id = ~escola + turma,
                                                     data = df_alunos, 
                                                     fpc = ~fpc )
 
+
+##### Amostargem Estratificada
+
+media_por_escola = df_alunos |>
+  group_by(escola) |>
+  summarise(alunos = n()) |>
+  pull(alunos) |>
+  mean()
+
+media_por_turma = df_alunos |>
+  group_by(turma) |>
+  summarise(alunos = n()) |>
+  pull(alunos) |>
+  mean()
+
+df_alunos |>
+  group_by(escola) |>
+  summarise(turmas = length(unique(turma))) |>
+  pull(turmas)|>
+  summary()
+
+select_from_n_cluster = function(n, var, ppt = F, tam = NULL){
+  
+  n_vars = length(var)
+  f = ifelse(
+    n_vars == 1, 
+    ifelse(
+      ppt,
+      function(x){
+        sampling::cluster(
+          x,
+          clustername = var,
+          size = 1,
+          method = c("poisson"),
+          pik =  sampling::inclusionprobabilities(
+            x |>
+              mutate(alunos = 1:n()) |>
+              group_by_at(var) |>
+              summarise_at(tam, ~length(unique(.))) |>
+              pull(tam), 1
+          )
+        ) |>
+          list()
+      },
+      function(x){
+        sampling::cluster(
+          x,
+          clustername = var,
+          size = 1,
+          method = c("srswor")
+        ) |>
+          list()
+      }), 
+    function(x){
+      sampling::mstage(
+        x, 
+        stage = rep("cluster", n_vars),
+        varnames = var,
+        size = lapply(rep(1, n_vars), function(x) x),
+        method = rep("srswor", n_vars)
+      )
+    }
+  )
+  indexes = f(df_alunos)
+  i = 0
+  while(i < n){
+    indexes_new = f(df_alunos[-indexes[[length(indexes)]]$ID_unit,])
+    if(is.null(indexes_new[[1]])) next
+    
+    indexes = map2(
+      indexes, 
+      indexes_new, 
+      function(x, y) rbind(x, y)
+    )
+    
+    i = nrow(indexes[[length(indexes)]]) 
+  }
+  
+  indexes$n = i
+  return(indexes)
+}
+
+
+set.seed(123)
+IACS =
+  list("escola", "turma", c("escola", "turma"), c("rede", "escola", "turma")) |>
+  map(~select_from_n_cluster(n = 750, .)) 
+
+IACS |>
+  map(~.$n)
+
+IACS_ppt =
+  mapply(
+    function(var, tam){ select_from_n_cluster(n = 750, var = var, ppt = T, tam = tam) },
+    var = c("escola", "escola"),
+    tam =  c("turma", "alunos"),
+    SIMPLIFY = F
+  ) |>
+  setNames(c("turma", "alunos"))
+
+IACS_ppt |>
+  map(~.$n)
+
+
+
+media_por_escola = df_alunos |>
+  group_by(escola) |>
+  summarise(alunos = n()) |>
+  pull(alunos) |>
+  mean()
+
+media_por_turma = df_alunos |>
+  group_by(turma) |>
+  summarise(alunos = n()) |>
+  pull(alunos) |>
+  mean()
+
+df_alunos |>
+  group_by(escola) |>
+  summarise(turmas = length(unique(turma))) |>
+  pull(turmas)|>
+  summary()
+
+select_from_n_cluster = function(n, var, ppt = F, tam = NULL){
+  
+  n_vars = length(var)
+  f = ifelse(
+    n_vars == 1, 
+    ifelse(
+      ppt,
+      function(x){
+        sampling::cluster(
+          x,
+          clustername = var,
+          size = 1,
+          method = c("poisson"),
+          pik =  sampling::inclusionprobabilities(
+            x |>
+              mutate(alunos = 1:n()) |>
+              group_by_at(var) |>
+              summarise_at(tam, ~length(unique(.))) |>
+              pull(tam), 1
+          )
+        ) |>
+          list()
+      },
+      function(x){
+        sampling::cluster(
+          x,
+          clustername = var,
+          size = 1,
+          method = c("srswor")
+        ) |>
+          list()
+      }), 
+    function(x){
+      sampling::mstage(
+        x, 
+        stage = rep("cluster", n_vars),
+        varnames = var,
+        size = lapply(rep(1, n_vars), function(x) x),
+        method = rep("srswor", n_vars)
+      )
+    }
+  )
+  indexes = f(df_alunos)
+  i = 0
+  while(i < n){
+    indexes_new = f(df_alunos[-indexes[[length(indexes)]]$ID_unit,])
+    if(is.null(indexes_new[[1]])) next
+    
+    indexes = map2(
+      indexes, 
+      indexes_new, 
+      function(x, y) rbind(x, y)
+    )
+    
+    i = nrow(indexes[[length(indexes)]]) 
+  }
+  
+  indexes$n = i
+  return(indexes)
+}
+
+
+set.seed(123)
+IACS =
+  list("escola", "turma", c("escola", "turma"), c("rede", "escola", "turma")) |>
+  map(~select_from_n_cluster(n = 750, .)) 
+
+IACS |>
+  map(~.$n)
+
+IACS_ppt =
+  mapply(
+    function(var, tam){ select_from_n_cluster(n = 750, var = var, ppt = T, tam = tam) },
+    var = c("escola", "escola"),
+    tam =  c("turma", "alunos"),
+    SIMPLIFY = F
+  ) |>
+  setNames(c("turma", "alunos"))
+
+IACS_ppt |>
+  map(~.$n)
+
+AESs_unif = sapply(1:1000, function(i) {
+  df_alunos |>
+    filter(aluno %in% IAESs_unif[,i]$x.ID_unit) |>
+    mutate(log_port = log(port), .keep = 'unused')
+}
+)
+
+fpc_calc = function(N,n){
+  ret = (((N-n)/(N-1))**(1/2))
+  return(ret)
+}
+
+AESs_unif_estimates = sapply(1:1000,function(i){
+  plan = svydesign(~1, strata=~rede, data = AESs_unif[,i] |> as.data.frame(), probs=~IAESs_unif[,i]$x.Prob)
+  svymean(~log_port,plan) |>
+    as.data.frame()
+}
+)
+
